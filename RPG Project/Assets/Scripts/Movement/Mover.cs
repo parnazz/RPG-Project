0ec710bc2bb276acb6b861b44e9455f1b113326f
parnaz <parnaz@mail.ru>
2020-08-10 13:49:15 +0300

    Section 2 - Done

---
+++ RPG Project/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private NavMeshAgent navMeshAgent;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+
+
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); ;
+        RaycastHit hitInfo;
+        bool isHitReturned = Physics.Raycast(ray, out hitInfo);
+        
+        if (isHitReturned)
+        {
+            navMeshAgent.destination = hitInfo.point;
+        }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        animator.SetFloat("forwardSpeed", speed);
+    }
+}
